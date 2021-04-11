6592e67dc12bd336c7cbdbc4b45dc478ec632ca5
Marco <msmo1960@gmail.com>
2021-04-11 09:06:43 -0500

    2nd commit

---
+++ Develop/script.js
@@ -0,0 +1,23 @@
+// Assignment code here
+const charset = {
+  number: '0123456789',
+  extended: '@%+\/!#$^?:,(){}[]~-_.<>',
+  uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  lowercase: 'abcdefghijklmnopqrstuvwxyz'
+}
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
